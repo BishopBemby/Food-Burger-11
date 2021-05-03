a06ae50558968af34fcd68857797d6c381d6e07b
Bishop Bemby <bishopbemby1995@gmail.com>
2021-05-03 20:29:54 +0530

    useEffect on cart button which bumps when a new item is added.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,35 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import CartContext from '../../store/cart-context';
 const HeaderCartButton = (props) => {
-
+const [btnBump, setBtnBump] =useState(false);
   const cartCtx = useContext(CartContext);
+const {items} = cartCtx;
+
+  useEffect(()=>{
+    if(cartCtx.items.length === 0){
+      return;
+    }
+    setBtnBump(true);
+
+    const timer = setTimeout(()=>{
+      setBtnBump(false);
+    }, 300);
+
+    return () =>{
+      clearTimeout(timer);
+    }
+  }, [items])
 
   const numberOfCartItems = cartCtx.items.reduce((currentNum, item)=>{
     return currentNum + item.amount;
   }, 0)
 
+const btnClasses = `${classes.button} ${btnBump ? classes.bump: ''}`
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
